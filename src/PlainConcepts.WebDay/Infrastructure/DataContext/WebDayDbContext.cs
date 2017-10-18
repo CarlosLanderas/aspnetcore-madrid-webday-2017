fc50b11b18648cf675fde6b9353ea345212bd228
Carlos Landeras Martínez <clanderas@plainconcepts.com>
2017-10-18 22:30:51 +0200

    New solution path and dbconfig!

--- src/PlainConcepts.WebDay/Infrastructure/DataContext/WebDayDbContext.cs
+++ src/PlainConcepts.WebDay/Infrastructure/DataContext/WebDayDbContext.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using PlainConcepts.WebDay.Model;
 
 namespace PlainConcepts.WebDay.Infrastructure.DataContext
 {
-    public class WebDayDbContext: DbContext
+    public class WebDayDbContext : DbContext
     {
-        public WebDayDbContext(DbContextOptions options) : base(options){}
+        public WebDayDbContext(DbContextOptions options) : base(options) { }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            
+            var userEntity = modelBuilder.Entity<User>();
+            userEntity.HasKey(u => u.Id);
+            userEntity.Property(u => u.Id)
+                .IsRequired();
+
+
         }
     }
 }
